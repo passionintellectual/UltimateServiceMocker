428af5400cc7234988bc165506bb4b9cc7b1bf09
gnemade <ganesh.nemade@xoriant.com>
2015-06-25 10:44:54 +0530

    added project.

---
+++ src/UltimateServiceMocker/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using UltimateServiceMocker ;
+
+namespace UltimateServiceMocker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Bootstrapper bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
+    }
+}
---
+++ src/UltimateServiceMocker/app_code/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Prism;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Prism.UnityExtensions;
+
+namespace UltimateServiceMocker
+{
+    public class Bootstrapper: UnityBootstrapper
+    {
+
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.MainWindow = (Window)Shell;
+        }
+    }
+}
