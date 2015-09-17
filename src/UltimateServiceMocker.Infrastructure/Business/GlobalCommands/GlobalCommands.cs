dd74cc6003dcf5f88855538915af7244179c477e
Ganesh Nemade <ganesh.nemade@xoriant.com>
2015-09-17 18:05:37 +0530

    Added ViewModelBase Updated Nuget package Added GlobalCompositeCommands

---
+++ src/UltimateServiceMocker.Infrastructure/Business/GlobalCommands/GlobalCommands.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Practices.Prism.Commands;
+
+namespace UltimateServiceMocker.Infrastructure.Business
+{
+    public static class GlobalCommands  
+    {
+        public static CompositeCommand FiddlerApplicationCloseCommand = new CompositeCommand();
+    }
+}
---
+++ src/UltimateServiceMocker.Infrastructure/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateServiceMocker.Infrastructure
+{
+    public class ViewModelBase: IViewModel, INotifyPropertyChanged
+    {
+        public IView View { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public ViewModelBase(IView view)
+        {
+            View = view;
+            View.ViewModel = this;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+         
+
+    }
+}
