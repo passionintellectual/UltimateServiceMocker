5d2f80ce867c85feee5f60beca74edf858f59650
Ganesh Nemade <ganesh.nemade@xoriant.com>
2015-09-22 20:06:48 +0530

    Refactoring code

---
+++ TabsModule/ViewModel/TabsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateServiceMocker.Infrastructure;
+
+namespace TabsModule.ViewModel
+{
+    public class TabsViewModel : ViewModelBase
+    {
+        public TabsViewModel(IView view):base(view)
+        {
+
+        }
+    }
+}
